1672822dcb483718f83d063f72c6bd5b14d31782
Veras-D <dveras2310@gmail.com>
2025-11-05 14:28:44 -0300

    fix(ci): Resolve linting and test errors in CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
-    ignores: ["dist/", "node_modules/"],
+    ignores: ["dist/", "node_modules/", "jest.config.ts", "tests/setup.ts"],
   },
   {
     files: ["**/*.ts"],
