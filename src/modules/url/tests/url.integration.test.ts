d52ca72203b352cd21b4710d2ef32223d43662be
Veras-D <dveras2310@gmail.com>
2025-11-10 11:54:47 -0300

    feat(url): Add a basic passing test to url.integration.test.ts

---
+++ src/modules/url/tests/url.integration.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest';
+import app from '../../../app';
+
+describe('URL Integration Tests', () => {
+  test('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
