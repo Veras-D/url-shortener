252458ac41adb09e959f36e0219e0d6bdcdb6184
Veras-D <dveras2310@gmail.com>
2025-11-11 02:59:47 -0300

    feat(common): add error handler to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import healthRoutes from '@modules/core/health.routes';
 import { urlRoutes } from '@modules/url/url.routes';
+import { errorHandler } from '@common/middlewares/errorHandler';
 
 dotenv.config();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 
 app.use('/health', healthRoutes);
 app.use('/api', urlRoutes);
+
+app.use(errorHandler);
 
 export default app;
