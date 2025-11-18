56f7ea9415a2f13a3903ae80c1385bcb9434620e
Matheus Moreira <mthsmoreira59@gmail.com>
2025-11-18 14:01:24 -0300

    feat(url): update userId validation to ensure it is non-empty

--- src/modules/url/url.validation.ts
+++ src/modules/url/url.validation.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const shortenUrlSchema = z.object({
   url: z.string().url({ message: 'Invalid URL format' }),
-  userId: z.string({ required_error: 'User ID is required' }),
+  userId: z.string().nonempty({ message: 'User ID is required' }),
 });
