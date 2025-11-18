834e98a997a98190e45c9e3e04fd7dfd12e6c5d1
Matheus Moreira <mthsmoreira59@gmail.com>
2025-11-18 16:04:17 -0300

    feat(tests): add setup file for database and Redis connection management

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./tests/jest.setup.ts'],
+  setupFilesAfterEnv: ['./tests/setup.ts'],
   moduleNameMapper: {
     '@config/(.*)': '<rootDir>/src/config/$1',
     '@libs/(.*)': '<rootDir>/src/libs/$1',
